f501356a23741c0a7a035b1034ca878df7e0bc59
Federico Allegrini <federico.allegrini.dev@gmail.com>
2021-01-09 23:01:52 +0100

    Filtering

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,14 @@
-function feed(parent, args, context, info) {
-  return context.prisma.link.findMany();
+async function feed(parent, args, context, info) {
+  const where = args.filter
+    ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } },
+        ],
+      }
+    : {};
+  const links = await context.prisma.link.findMany({ where });
+  return links;
 }
 async function link(parent, args, context, info) {
   const link = await context.prisma.link.findUnique({
